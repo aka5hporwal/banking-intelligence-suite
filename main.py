5b6e6ff6ec37138d6caacf86b524ec32db830a11
Akash Porwal <akash.compound@gmail.com>
2025-07-15 10:04:05 +0530

    prepare base model changes

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from intelligentBanking import logger
 from intelligentBanking.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from intelligentBanking.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 # This script is the entry point for the Intelligent Banking project.
-if __name__ == "__main__":
-    try:
-        logger.info("Starting the Intelligent Banking pipeline...")
-        pipeline = DataIngestionTrainingPipeline()
-        pipeline.main()
-        logger.info("Intelligent Banking pipeline completed successfully.")
-    except Exception as e:
-        logger.exception("An error occurred in the Intelligent Banking pipeline.")
-        raise e
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    pipeline = DataIngestionTrainingPipeline()
+    pipeline.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    prepare_pipeline = PrepareBaseModelTrainingPipeline()
+    prepare_pipeline.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
 
 
 
